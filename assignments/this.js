306a7f58cdde745e233c9d6c61d47f27559ea58e
Crystal Martin <drcrystalm@gmail.com>
2019-04-16 21:39:40 -0500

    finished binding examples

--- assignments/this.js
+++ assignments/this.js
@@ -20,18 +20,64 @@
 
 // Principle 1
 
+var sayAge = function () {
+    console.log(this.age);
+};
 
+var me = {
+    age: 25
+};
+
+sayAge();
 
 // code example for Window Binding
 
 // Principle 2
 
+var me = {
+    name: 'Crystal',
+    age: 39,
+    sayName: function () {
+        console.log(this.name + ` says hi!`);
+    }
+};
+me.sayName()
+
 // code example for Implicit Binding
 
 // Principle 3
 
-// code example for New Binding
+function User(nameString, emailString, passwordString) {
+    this.name = nameString;
+    this.email = emailString;
+    this.password = passwordString;
+    this.saysHello = function () {
+        return `Hello ${this.name} !`
+    }
+}
+
+
+//This may look impressive, but I had to copy it from my old precoursework notes:) 
+
+let userSara = new User('Sara', 'sara@gmail.com', '123abc');
+console.log(userSara);
+console.log(userSara.saysHello());
+
+// code example for New Binding - SO I did the above example for both.
+
+
 
 // Principle 4
 
+var sayName = function () {
+    console.log(`My name is ${this.name}`);
+}
+var susan = {
+    name: 'Susan',
+    age: 60,
+}
+sayName.call(susan);
+
+
+// For my notes- Explicit uses CALL, APPLY, or BIND!
 // code example for Explicit Binding
